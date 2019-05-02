0f85d0b8a73cf90807575e335db360fb63f79140
rpavliuk <pavliuk.roman@pdffiller.team>
2019-05-02 12:18:23 +0300

    Remove title

--- app/src/main/java/design/skullper/com/designtest/screens/AuthActivity.kt
+++ app/src/main/java/design/skullper/com/designtest/screens/AuthActivity.kt
@@ -22,7 +22,7 @@ class AuthActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
-        supportActionBar?.title = "Some title"
+        supportActionBar?.title = ""
         tv_auth_label_sign_in.text = getString(R.string.auth_sign_in_label).withHtml()
     }
 }
